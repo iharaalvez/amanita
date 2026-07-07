8a4c912df3a081f717874778211172cf8c00dc16
Ihara <iharaalvez@users.noreply.github.com>
2026-07-07 03:31:43 -0300

    fix: reorder Snacksworth Legendaries sub-dex by National Dex number

---
+++ supabase/migrations/202607070002_scarlet_violet_snacksworth_national_dex_order.sql
@@ -0,0 +1,33 @@
+-- Reorder the Snacksworth Legendaries sub-dex by National Dex number instead
+-- of the Scarlet/Violet-exclusive/shared/group-quest grouping used when they
+-- were first seeded.
+
+insert into public.game_dex_entries (game_id, species_id, form_name, entry_number)
+values
+  ('scarlet-violet-snacksworth', 144, '', 1),  -- Articuno
+  ('scarlet-violet-snacksworth', 145, '', 2),  -- Zapdos
+  ('scarlet-violet-snacksworth', 146, '', 3),  -- Moltres
+  ('scarlet-violet-snacksworth', 243, '', 4),  -- Raikou
+  ('scarlet-violet-snacksworth', 244, '', 5),  -- Entei
+  ('scarlet-violet-snacksworth', 245, '', 6),  -- Suicune
+  ('scarlet-violet-snacksworth', 249, '', 7),  -- Lugia
+  ('scarlet-violet-snacksworth', 250, '', 8),  -- Ho-Oh
+  ('scarlet-violet-snacksworth', 380, '', 9),  -- Latias
+  ('scarlet-violet-snacksworth', 381, '', 10), -- Latios
+  ('scarlet-violet-snacksworth', 382, '', 11), -- Kyogre
+  ('scarlet-violet-snacksworth', 383, '', 12), -- Groudon
+  ('scarlet-violet-snacksworth', 384, '', 13), -- Rayquaza
+  ('scarlet-violet-snacksworth', 638, '', 14), -- Cobalion
+  ('scarlet-violet-snacksworth', 639, '', 15), -- Terrakion
+  ('scarlet-violet-snacksworth', 640, '', 16), -- Virizion
+  ('scarlet-violet-snacksworth', 643, '', 17), -- Reshiram
+  ('scarlet-violet-snacksworth', 644, '', 18), -- Zekrom
+  ('scarlet-violet-snacksworth', 646, '', 19), -- Kyurem
+  ('scarlet-violet-snacksworth', 791, '', 20), -- Solgaleo
+  ('scarlet-violet-snacksworth', 792, '', 21), -- Lunala
+  ('scarlet-violet-snacksworth', 800, '', 22), -- Necrozma
+  ('scarlet-violet-snacksworth', 891, '', 23), -- Kubfu
+  ('scarlet-violet-snacksworth', 896, '', 24), -- Glastrier
+  ('scarlet-violet-snacksworth', 897, '', 25)  -- Spectrier
+on conflict (game_id, species_id, form_name)
+do update set entry_number = excluded.entry_number;
